2d7089dc87bf94de461414830603695bf1a93ccc
minecire <minecirecire@gmail.com>
2019-11-26 17:42:51 -0500

    stuff'n'things

--- bot.js
+++ bot.js
@@ -8,22 +8,39 @@ var currentLine;
 var question;
 var answering = false;
 var answer;
+var players = [];
 
 
 client.on('ready', () => {  
-    console.log(`Logged in as ${client.user.tag}!`);
+    console.log('Logged in as ${client.user.tag}!');
 });
 
 client.on('message', message => {
     if (message.author == client.user){
         return;
     }
+    var currentPlayer;
+    var addName = true;
+    for(var i = 0; i < players.length; i++){
+        if(players[i].id == message.author.id){
+            addName = false;
+            currentPlayer = players[i];
+        }
+    }
+    if(addName){
+        currentPlayer = {Name:message.author.username, id:message.author.id, Correct:0, Incorrect:0, OutOfTime:0, Cancelled:0};
+        players.push(currentPlayer);
+    }
     // sf.get(`https://www.reddit.com/r/trivia/random.json?limit=1`).then(res => {
     // message.channel.send(res.body[0].data.children[0].data.selftext);
     // //console.log(res.body[1].data.children);
 
     // });
-    if(message.content == "triv q"){
+    if(message.content == "triv q" || message.content == "triv question"){
+        if(answering){
+            message.channel.send("```Previous Question cancelled");
+            currentPlayer.Cancelled++;
+        }
         fs.readFile('TriviaQ.txt', 'utf-8', (err, data) => {
             if (err) throw err;
             var dataSplit = data.split('\n');
@@ -45,21 +62,35 @@ client.on('message', message => {
             }
             answer = lineSplit[i];
             answering = true;
-            message.channel.send(question);
+            message.channel.send("```Trivia question #"+currentTriv+"\n"+question+"```");
             setTimeout(function(){
                 if(answering){
-                    message.channel.send("Out of time. The correct answer was "+answer);
+                    message.channel.send("```Out of time. The correct answer was "+answer+"```");
+                    currentPlayer.OutOfTime++;
                     answering = false;
                 }
             },30000);
         });
     }
     else if(answering){
-        if(message.content.toLowerCase() == answer.toLowerCase()){
-            message.channel.send("Correct! The answer was "+answer);
+        if(message.content == "cancel"){
+            message.channel.send("```Trivia question cancelled. The answer was "+answer+"```");
+            currentPlayer.Cancelled++;
             answering = false;
+            
         }
+        else if(message.content.toLowerCase() == answer.toLowerCase()){
+            message.channel.send("```Correct! The answer was "+answer+"```");
+            answering = false;
+            currentPlayer.Correct++;
+        }
+        else{
+            currentPlayer.Incorrect++;
+        }
+    }
+    if(message.content == "triv stats" || message.content == "triv s"){
+        message.channel.send("```Stats for "+currentPlayer.Name+"\n\nAnswered Correctly "+currentPlayer.Correct+" times \nAnswered Incorrectly "+currentPlayer.Incorrect+" times \nRan out of time "+currentPlayer.OutOfTime+" times\nCancelled a question "+currentPlayer.Cancelled+" times```")
     }
 });
 
-client.login('key');
+client.login('NjQ4OTI1NTk3OTU5MTkyNTk4.Xd2jeQ.FkfH-2WjysDb_ltP6MoVipVkceU');
